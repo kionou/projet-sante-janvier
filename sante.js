e8c6435369dafa9c43fd0abdafef7b954e78fc1b
kionou <kionoumamadou.00@gmail.com>
2022-01-24 16:52:35 +0000

    deux

--- sante.js
+++ sante.js
@@ -1,16 +1,15 @@
-let apie = document.querySelector('.api')
-let key='16908a9acc764c4c877354d843e8834b';
+// let apie = document.querySelector('.api')
+// let key='16908a9acc764c4c877354d843e8834b';
 
 
-const data = async()  =>{
- covid = await  fetch(`https://covid19.mathdro.id/api`)
-.then(res => res.json())
-console.log(covid);
+
+// fetch(`https://covid-19-statistics.p.rapidapi.com/regions`)
+// .then(res => res.json())
 // .then(data => {
 //     console.log(data);
 // });
-}
-data();
+
+
 
 
 
@@ -18,3 +17,16 @@ data();
 
 // https://data.gouv.ci/data-fair/api/v1/datasets/sites-de-vaccination-de-covid-19-dans-le-district-d'abidjan/
 
+fetch("https://covid-19-fastest-update.p.rapidapi.com/summary", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid-19-fastest-update.p.rapidapi.com",
+		"x-rapidapi-key": "69110c8a4amsh0658ef5da418e7fp14727bjsnb22e39f4d02c"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});

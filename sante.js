267221ca4cfc6de167166c129774614efe9d243e
kionou <kionoumamadou.00@gmail.com>
2022-01-26 17:44:24 +0000

    presque

--- sante.js
+++ sante.js
@@ -3,30 +3,16 @@
 
 
 
-// fetch(`https://covid-19-statistics.p.rapidapi.com/regions`)
-// .then(res => res.json())
-// .then(data => {
-//     console.log(data);
-// });
-
-
-
-
-
-// https://koumoul.com/data-fair/embed/dataset/sites-de-vaccination-de-covid-19-dans-le-district-d'abidjan/thumbnails"
-
-// https://data.gouv.ci/data-fair/api/v1/datasets/sites-de-vaccination-de-covid-19-dans-le-district-d'abidjan/
-
-fetch("https://covid-19-fastest-update.p.rapidapi.com/summary", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "covid-19-fastest-update.p.rapidapi.com",
-		"x-rapidapi-key": "69110c8a4amsh0658ef5da418e7fp14727bjsnb22e39f4d02c"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
+fetch(` https://koumoul.com/data-fair/embed/dataset/sites-de-vaccination-de-covid-19-dans-le-district-d'abidjan/thumbnails`)
+.then(res => res.json())
+.then(data => {
+    console.log(data);
 });
+
+
+
+
+
+
+
+

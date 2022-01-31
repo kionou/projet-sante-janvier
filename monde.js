3a6d62f11841c58994be43c823e483e24fad31ac
kionou <kionoumamadou.00@gmail.com>
2022-01-31 10:27:07 +0000

    ffff

--- monde.js
+++ monde.js
@@ -1,68 +1,12 @@
- let cote = document.querySelector('.cas1')
  let monde11 = document.querySelector('.monde11');
  let monde222 = document.querySelector('.monde222');
- let search = document.querySelectorAll('input');
+ let search = document.querySelector('input');
  console.log(search);
 
 
-//  LOCALSTRAGE
 
 
-
-
-
-fetch(`https://disease.sh/v3/covid-19/countries/cote%20d%27ivoire?strict=true`)
-.then(res => res.json())
-.then(data => {
-    console.log(data);
-    cote.innerHTML = `
-     <div class="row">
-                    <div class="col-12 col-lg-4 col-md-4">
-                      <div class="jumbotron rouge shadow-lg p-3 mb-5 bg-body rounded ">
-                                <p class="text-center">${data.cases}</p>
-                                <h6 class="text-center">Cas confirmés au total</h6>
-                      </div>
-                    </div>
-                    <div class="col-12 col-lg-4 col-md-4">
-                      <div class="jumbotron orange shadow-lg p-3 mb-5 bg-body rounded">
-                        <p class="text-center">${data.active}</p>
-                        <h6 class="text-center">Cas actifs</h6>
-                      </div>
-                    </div>
-                    <div class="col-12 col-lg-4 col-md-4">
-                      <div class="jumbotron vert shadow-lg p-3 mb-5 bg-body rounded">
-                        <p class="text-center">${data.recovered}</p>
-                        <h6 class="text-center">Cas guéris</h6>
-                      </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-12 col-lg-4 col-md-4">
-                      <div class="jumbotron noir shadow-lg p-3 mb-5 bg-body rounded">
-                        <p class="text-center">${data.deaths} <br>Décès </p>
-                        <h6 class="text-center">Décès</h6>
-                      </div>
-                    </div>
-                    <div class="col-12 col-lg-4 col-md-4">
-                      <div class="jumbotron bleu shadow-lg p-3 mb-5 bg-body rounded">
-                        <p class="text-center">${data.tests}</p>
-                        <h6 class="text-center">Nombre total de tests</h6>
-                      </div>
-                    </div>
-
-                    <div class="col-12 col-lg-4 col-md-4">
-                      <div class="jumbotron shadow-lg p-3 mb-5 bg-body rounded">
-                        <h5 class="text-center">${data.updated}</h5>
-                         <p class="text-center">gjyjgyjyj</p>
-                      </div>
-                    </div>
-                </div>
-
-    `
-});
-
-
+ 
 fetch(`https://disease.sh/v3/covid-19/all
 `)
 .then(res => res.json())
@@ -147,5 +91,36 @@ fetch(`https://disease.sh/v3/covid-19/countries`)
         
     }
 })
+  
+let recherche = '';
+search.addEventListener('input' , (e)   =>{
+    e.preventDefault();
+    recherche = e.target.value
+    console.log(recherche);
+  fetch(`https://disease.sh/v3/covid-19/countries/${recherche}?strict=true`)
+.then(res => res.json())
+.then(word => {
+    console.log(word);
+     monde222.innerHTML = " ";
 
+       monde222.innerHTML +=  ` 
+                        
+                        <td class="text-center" data-label="pays">${word.country}</td>
+                        <td class="text-center" data-label="cas confirmes">${word.cases}</td>
+                        <td class="text-center" data-label="guerri">${word.recovered}</td>
+                        <td class="text-center" data-label="morts">${word.deaths}</td>
+                        <td class="text-center" data-label="cas actifs">${word.active}</td>
+                        <td class="text-center" data-label="tests ">${word.tests}</td>
+        
+        
+        `
+    //  for (let i = 0; i < word.length; i++) {
+    //     const element = word[i];
+      
+
+        
+    // }
+})
+
+})
 
